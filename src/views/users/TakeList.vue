49a57f2b7d292c0cb1d2dc537fd7507a0854a7c5
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-03-30 17:30:29 +0900

    issu

--- src/views/users/TakeList.vue
+++ src/views/users/TakeList.vue
@@ -332,29 +332,4 @@ function findKeyWord(name){
     </table>
 
 
-    <pagination
-    v-model="page"
-    :options="{ template: MyPagination }"
-    :per-page="PER_PAGE"
-    :records="totalPages"
-    @paginate="onPageClick"
-  />
-
 </template>
-
-
-<script>
-import MyPagination from "@/views/users/MyPagination";
-export default {
-  name: "HelloWorld",
-  setup() {
-    const PER_PAGE = 3;
-    const page = ref(1);
-    const totalPages = ref(20);
-    const onPageClick = (pageNumber) => {
-      console.log(pageNumber);
-    };
-    return { totalPages, MyPagination, onPageClick, page, PER_PAGE };
-  },
-};
-</script>
