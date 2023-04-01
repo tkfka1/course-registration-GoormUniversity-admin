10221107d768663292d7906b69496a42234fbbb3
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-04-01 16:27:15 +0900

    test

--- src/main.js
+++ src/main.js
@@ -2,10 +2,8 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import App from './App.vue';
 import { router } from './router';
-import Paginate from 'vuejs-paginate'
 
 const app = createApp(App);
-app.component('paginate', Paginate)
 app.use(createPinia());
 app.use(router);
 app.mount('#app');
