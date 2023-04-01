13354be55ed587388fc1dea431a481f1b2a78e7c
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-04-01 19:26:42 +0900

    test api update

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
     server:{
       proxy:{
         "/api":{ 
-          target: "http://localhost:8080",
+          target: "http://localhost:8080/api/",
           rewrite: (path)=>path.replace(/^\/api/,""),
         },
       }
