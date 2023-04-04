3c3ccc5c5333f93af66c324933b894d3e4642846
hankyo <36651040+tkfka1@users.noreply.github.com>
2023-04-04 19:33:08 +0900

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -22,14 +22,14 @@ fetchWrapper.get(`/api/admin/auth/${user._object.user.id}`).then((res) => {
 
 <template>
     <div v-if="user">
-        <h1>안녕하세요 {{ name }}!</h1>
+        <h1>안녕하세요 {{ name }}님!</h1>
         <p>구름대학 수강신청에 오신것을 환영합니다.</p>
-        <p><router-link to="/users">test1</router-link></p>
-
-        <td>{{ adminId }}</td>
-        <td>{{ name }}</td>
-
-                    <p>dsa</p>    
+        <p><router-link to="/admin">교직원</router-link></p>
+        <p><router-link to="/users">학생</router-link></p> 
+        <p><router-link to="/professor">교수</router-link></p> 
+        <p><router-link to="/major">전공</router-link></p> 
+        <p><router-link to="/lecture">강의</router-link></p> 
+        <p><router-link to="/openTime">신청시간</router-link></p> 
 
     </div>
 
